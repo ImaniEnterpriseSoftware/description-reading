73b9467c8f86f48f53e0ddc22b6d3e5286ff36d8
ImaniLeemans <455181@student.fontys.nl>
2023-05-09 13:32:24 +0200

    Initial commit

---
+++ description-R/src/test/java/com/example/descriptionr/DescriptionRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.descriptionr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DescriptionRApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
