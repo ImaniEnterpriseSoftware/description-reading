df71d99f89442d5b5e9109bf6801493a6d3c655f
ImaniLeemans <455181@student.fontys.nl>
2023-05-19 16:58:11 +0200

    Works on docker

--- description-R/src/main/java/com/example/descriptionr/config/ModelMapperConfig.java
+++ description-R/src/main/java/com/example/descriptionr/config/ModelMapperConfig.java
@@ -1,8 +1,5 @@
 package com.example.descriptionr.config;
 
-import com.example.descriptionr.model.Description;
-import com.example.descriptionr.model.DescriptionDTO;
-import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
